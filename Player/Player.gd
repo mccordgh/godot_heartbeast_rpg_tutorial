0610141ac37ebd9524217949c729fa6b88f84cfe
Matthew McCord <mattm.eternal@gmail.com>
2022-08-13 19:24:03 -0700

    video 9

--- Player/Player.gd
+++ Player/Player.gd
@@ -9,10 +9,31 @@ onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var animation_state = animation_tree.get("parameters/playback")
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
 var velocity = Vector2.ZERO
 
 
+func _ready():
+	animation_tree.active = true
+
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			roll_state(delta)
+		ATTACK:
+			attack_state(delta)
+	
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -23,7 +44,9 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	# Player is not moving
 	else:
@@ -31,3 +54,18 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+
+func attack_state(delta):
+	animation_state.travel("Attack")
+	
+	
+func back_to_move_state():
+	state = MOVE
+	
+	
+func roll_state(delta):
+	pass
